31101e5a5b2df0fc80c63d883b491cbe8e1a84dd
21PD23@cs2k16.amcs.ws2k12.psgcs.edu <21PD23@cs2k16.amcs.ws2k12.psgcs.edu>
2022-05-12 18:11:30 +0530

    hello

--- example.py
+++ example.py
@@ -1,10 +1,39 @@
-from flask import Flask
+from flask import Flask , request 
 
 app = Flask(__name__)
+
+student = {
+    "1" : {"Name" : "Jagadeesh","RollNo" : "21pd13"},
+    "2" : {"Name" : "Nithish","RollNo" : "21pd23" },
+    "3" : {"Name" : "Rohith","RollNo" : "21pd38"}
+    }
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/student")
+def display():
+    return student
+
+@app.route("/student/<id>")
+def search(id):
+    return student[id]
+
+@app.route("/student/search/<id>")
+def search1(id):
+    for i in student :
+        if student[i]["RollNo"]==id :
+            return student[i]["Name"]
+    return "<p>ID NOT FOUND !!!</p>"
+
+@app.route("/insert",methods = ["POST"])
+def insert():
+    if request.method == "POST" :
+        for key,value in request.json.items() :
+            print (key,value)
+            student[key]=value
+    return student 
 
 if __name__ == "__main__" :
     app.run()
